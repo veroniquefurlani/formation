6fa4339320a05667d64ff309d9c1fd60504c27c3
vero <veronique.furlani@gmail.com>
2018-01-08 14:18:13 +0100

    let et const

---
+++ js/exercice3.js
@@ -0,0 +1,15 @@
+var a = 1;  // A PROSCRIRE !
+console.log(a);
+
+// LET ET cONST
+let b = 2;
+const c = 3;
+console.log(b);
+console.log(c);
+
+{
+    var d = 2;
+    let e = 2;
+}
+console.log(d);
+console.log(e);
